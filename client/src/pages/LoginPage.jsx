f38b0b1bbe026e3c9baf498b55e4d877269a6977
RohitBeniwal <beniwalrb177@gmail.com>
2023-09-30 23:19:56 +0530

    update

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -9,7 +9,7 @@ export default function LoginPage (){
                 <input type="email" placeholder='your@email.com' />
                 <input type="password" placeholder="password" />
                 <button className="primary">Login</button>
-                <div className="text-center py-2">Don't have an account yet? <Link to={'/register'}>Register Now</Link>
+                <div className="text-center py-2 text-gray-500">Don't have an account yet? <Link className="underline text-black" to={'/register'}>Register Now</Link>
                 </div>
             </form>
             </div>
