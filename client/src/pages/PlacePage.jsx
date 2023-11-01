bb91c5e921ef63a6a743f95ae33c61bba50a4d4e
RohitBeniwal <beniwalrb177@gmail.com>
2023-11-01 12:16:40 +0530

    update 12:16 01/11/23

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import BookingWidget from "../BookingWidget";
 
 export default function PlacePage() {
   const { id } = useParams();
@@ -19,15 +20,25 @@ export default function PlacePage() {
 
   if (showAllPhotos) {
     return (
-      <div className="absolute inset-0 bg-white min-h-screen">
-        <div className="p-8 grid gap-4">
+      <div className="absolute inset-0 bg-black text-white min-h-screen">
+        <div className="bg-black p-8 grid gap-4">
           <div>
-            <button>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-  <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
-</svg>
-
-                Close photos</button>
+            <h2 className="text-3xl">Photos of {place.title}</h2>
+            <button onClick={()=>setShowAllPhotos(false)} className="flex gap-1 py-2 px-4 rounded-2xl fixed shadow shadow-black right-12 top-8 bg-white text-black">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              Close photos
+            </button>
           </div>
           {place?.photos?.length > 0 &&
             place.photos.map((photo) => (
@@ -41,18 +52,23 @@ export default function PlacePage() {
   }
 
   return (
-    <div className="mt-4 bg-gray-100 -mx-8 px-8 py-8">
+    <div className="mt-4 bg-gray-100 -mx-8 px-8 pt-8">
       <h1 className="text-3xl">{place.title}</h1>
       <a
-        className="my-2 block font-semibold underline"
+        className="flex gap-1 my-3 block font-semibold underline"
         target="_blank"
         rel="noreferrer"
         href={"https://maps.google.com/?q=" + place.address}
       >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+</svg>
+
         {place.address}
       </a>
       <div className="relative">
-        <div className="grid grid-cols-[2fr_1fr] gap-2">
+        <div className="grid grid-cols-[2fr_1fr] gap-2 rounded-3xl overflow-hidden">
           <div>
             {place.photos?.[0] && (
               <div>
@@ -101,6 +117,29 @@ export default function PlacePage() {
           </svg>
           Show more photos
         </button>
+      </div>
+      <div className="mt-8 mb-8 gap-8 grid grid-cols-1 md:grid-cols-[2fr_1fr]">
+        <div>
+      <div className="my-4">
+        <h2 className="text-2xl font-semibold">Description</h2>
+        {place.description}
+      </div>
+          Check-In: {place.checkIn}<br/>
+          Check-Out: {place.checkOut}<br/>
+          Max Number Of Guests: {place.maxGuests}
+        </div>
+        <div>
+          <BookingWidget place={place}/>
+        </div>
+      </div>
+      <div className="bg-white -mx-8 px-8 py-8 border-t" >
+
+      <div>
+      <h2 className="text-2xl font-semibold">Extra Info</h2>
+      </div>
+          <div className="mb-4 mt-1 text-sm text-gray-700 leading-4">
+            {place.extraInfo}
+          </div>
       </div>
     </div>
   );
